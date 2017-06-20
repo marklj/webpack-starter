b46c02ea1c1c5da6d14c8be0fd0aaee16061f6fd
Mark Johnson <marklj@gmail.com>
2017-06-20 14:18:04 -0700

    modify config for better readability

--- webpack.config.js
+++ webpack.config.js
@@ -4,42 +4,54 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
+
   entry: {
     app: './src/index.js',
     vendor: ['lodash']
   },
+
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/'
   },
+
   devServer: {
     hot: true, // Tell the dev-server we're using HMR
     contentBase: path.resolve(__dirname, 'dist'),
     publicPath: '/'
   },
+
   module: {
    rules: [
+
+     // styles / css
      {
        test: /\.css$/,
        use: ExtractTextPlugin.extract({
             use: 'css-loader'
         })
      },
+
+     // images
      {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           'file-loader'
         ]
       },
+
+      // fonts
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           'file-loader'
         ]
       }
+
    ]
  },
+
  plugins: [
   new HtmlWebpackPlugin({
     title: 'Page Title',
@@ -51,4 +63,5 @@ module.exports = {
       name: 'vendor'
   })
  ]
+ 
 };
