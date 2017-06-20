e1b3ddea523bc8676c45d74a89f153885b1bafb2
Mark Johnson <marklj@gmail.com>
2017-06-20 15:35:46 -0700

    add postcss support with SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,13 @@ module.exports = {
      {
        test: /\.css$/,
        use: ExtractTextPlugin.extract({
-            use: 'css-loader'
+         use: [
+             {
+               loader: 'css-loader',
+               options: { importLoaders: 1 },
+             },
+             'postcss-loader',
+           ],
         })
      },
 
@@ -63,5 +69,5 @@ module.exports = {
       name: 'vendor'
   })
  ]
- 
+
 };
